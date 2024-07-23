6ec75d97b7c79edfb07efef72b5163134e1dd1b8
Nate <46688736+grumpy-photographer@users.noreply.github.com>
2024-07-22 23:22:56 -0400

    formatting

--- rocket_launch.py
+++ rocket_launch.py
@@ -6,18 +6,16 @@ import os
 
 os.system("cls")
 
-weather_url = "https://api.weather.gov/"
 rocket_launch_data_url = "https://lldev.thespacedevs.com/2.2.0/launch/upcoming"
 
 rocket_launch = get_results(rocket_launch_data_url)
 
 rocket_launch_data = pd.DataFrame(rocket_launch).reset_index().drop("index", axis=1)
 
-rocket_launch_data['key'] = rocket_launch_data.index
+rocket_launch_data["key"] = rocket_launch_data.index
 
 rocket_launch_data["Launch Date"] = rocket_launch_data["Launch Date"].astype(str)
 rocket_launch_data["Launch Time"] = rocket_launch_data["Launch Time"].astype(str)
-
 
 
 weather_data = pd.DataFrame()
@@ -25,7 +23,6 @@ weather_data = pd.DataFrame()
 for i in rocket_launch_data.index:
 
     weather = get_weather(
-        weather_url,
         rocket_launch_data["Latitude"][i],
         rocket_launch_data["Longitude"][i],
         (rocket_launch_data["Launch Date"][i]),
@@ -41,7 +38,9 @@ launch_data = pd.merge(rocket_launch_data, weather_data, how="left", on="key")
 launch_data = launch_data.drop(columns=["key"], axis=1)
 
 print(
-    "Weather Data for Next {} Rocket Launches in the USA\n\n".format(launch_data.shape[0]),
+    "Weather Data for Next {} Rocket Launches in the USA\n\n".format(
+        launch_data.shape[0]
+    ),
     tabulate(launch_data, headers="keys", tablefmt="fancy_grid"),
     sep="",
 )
--- weather.py
+++ weather.py
@@ -1,48 +1,49 @@
-# %%
 import pandas as pd
 import requests
 import json
 
-pd.set_option("display.max_columns", None)
-pd.set_option("display.max_rows", None)
-
-# %%
 base_url = "https://api.weather.gov/"
 
-# %% [markdown]
-# ***
 
-
-# %%
 def get_weather(base_url, lat, lon, date, time):
+    base_url = base_url
+
     day_filter = date
     time_filter = time
 
     geo_data = pd.json_normalize(
-        json.loads(
-            requests.get(
-                base_url + "points/{},{}".format(lat, lon)
-                ).text
-            )
-        )['properties.forecastHourly'][0]
-    
-    grid_data = pd.json_normalize(
-        json.loads(
-            requests.get(
-                geo_data
-                ).text
-            )
-        )['properties.periods'][0]
-    
+        json.loads(requests.get(base_url + "points/{},{}".format(lat, lon)).text)
+    )["properties.forecastHourly"][0]
+
+    grid_data = pd.json_normalize(json.loads(requests.get(geo_data).text))[
+        "properties.periods"
+    ][0]
+
     hourly_forecast_table = pd.json_normalize(grid_data)
 
-    hourly_forecast_table['date'] = pd.to_datetime(hourly_forecast_table['startTime']).dt.date.astype(str)
-    hourly_forecast_table['time'] = pd.to_datetime(hourly_forecast_table['startTime']).dt.time.astype(str)
+    hourly_forecast_table["date"] = pd.to_datetime(
+        hourly_forecast_table["startTime"]
+    ).dt.date.astype(str)
+    hourly_forecast_table["time"] = pd.to_datetime(
+        hourly_forecast_table["startTime"]
+    ).dt.time.astype(str)
 
-    date_forecast = hourly_forecast_table[hourly_forecast_table['date'] == day_filter]
-    hour_forecast = date_forecast[date_forecast['time'] == time_filter]
+    date_forecast = hourly_forecast_table[hourly_forecast_table["date"] == day_filter]
+    hour_forecast = date_forecast[date_forecast["time"] == time_filter]
 
-    columns_to_keep = ['date', 'time', 'isDaytime', 'shortForecast', 'temperature', 'temperatureUnit', 'windSpeed', 'windDirection', 'probabilityOfPrecipitation.value', 'dewpoint.value', 'relativeHumidity.value']
+    columns_to_keep = [
+        "date",
+        "time",
+        "isDaytime",
+        "shortForecast",
+        "temperature",
+        "temperatureUnit",
+        "windSpeed",
+        "windDirection",
+        "probabilityOfPrecipitation.value",
+        "dewpoint.value",
+        "relativeHumidity.value",
+    ]
     forecast = hour_forecast[columns_to_keep]
 
     forecast = forecast.rename(
@@ -53,20 +54,5 @@ def get_weather(base_url, lat, lon, date, time):
             "relativeHumidity.value": "Humidity",
         }
     )
-    
+
     return forecast
-
-
-# %% [markdown]
-# ***
-
-# %% [markdown]
-# Cape Canaveral, FL, USA
-
-# %%
-# Starlink Group 6-11
-# get_weather(base_url, 28.561941, -80.577357, '2023-08-23', '00:00:00')
-
-# %%
-# Starlink Group 7-1
-# get_weather(base_url, 28.561941, -80.577357, '2023-08-22', '06:00:00')
